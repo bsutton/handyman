dbb57f4001e8bf0731327ea803ef1bfd68ffa6c0
Brett Sutton <bsutton@noojee.com.au>
2023-12-07 16:16:54 +1100

    first commit

---
+++ js/booking.js
@@ -0,0 +1,4 @@
+submitBooking = function (event) {
+    let bookingForm = document.getElementById('booking-form');
+    bookingForm.submit();
+}
---
+++ js/modal.js
@@ -0,0 +1,59 @@
+
+// Stores the currenlty open modal dialog.
+// Will be null if no modal dialog is open.
+let openModal = null;
+
+// show the modal dialog.
+showModal = async function (event, contentUrl) {
+    event.preventDefault();
+    const html = await loadHtml(contentUrl);
+    openModal = document.getElementById("modal-dialog");
+
+    let container = document.getElementById("modal-container");
+    container.innerHTML = html;
+
+    // show the dialog
+    openModal.classList.remove("modal-box-closed");
+    openModal.classList.add("modal-box-open");
+    // obscure the main page
+    let modalOverlay = document.getElementById("modal-overlay");
+    modalOverlay.style.display = 'block';
+    return false;
+}
+
+// When the user clicks anywhere outside of the modal, close it
+closeModal = function (event, dialogId) {
+    event.preventDefault();
+    if (openModal !== null) {
+        openModal.classList.remove("modal-box-open");
+        openModal.classList.add("modal-box-closed");
+        openModal = null;
+        // un-obscure the main page
+        let modalOverlay = document.getElementById("modal-overlay");
+        modalOverlay.style.display = 'none';
+
+    }
+}
+
+// load the contents of the modal from url
+loadHtml = async function (url) {
+    const response = await fetch(url /*, options */);
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.text();
+    return data;
+}
+
+
+// show the booking dialog
+showBooking = async function (event) {
+    closeModal(event);
+    showModal(event, 'booking.html');
+}
+
+// show the booking dialog
+showAccept = async function (event) {
+    showModal(event, 'accept.html');
+}
