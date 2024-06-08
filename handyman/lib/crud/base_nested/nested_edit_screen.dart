a46dc6a9bb9cca80746b04eb6fda9f7ff8379445
Brett Sutton <bsutton@noojee.com.au>
2024-06-08 23:18:49 +1000

    formatting.

--- handyman/lib/crud/base_nested/nested_edit_screen.dart
+++ handyman/lib/crud/base_nested/nested_edit_screen.dart
@@ -11,18 +11,19 @@ abstract class NestedEntityState<E extends Entity<E>> {
 
 class NestedEntityEditScreen<C extends Entity<C>, P extends Entity<P>>
     extends StatefulWidget {
-  const NestedEntityEditScreen(
-      {required this.entity,
-      required this.editor,
-      required this.onInsert,
-      required this.entityName,
-      required this.entityState,
-      required this.dao,
-      super.key});
+  const NestedEntityEditScreen({
+    required this.entity,
+    required this.editor,
+    required this.onInsert,
+    required this.entityName,
+    required this.entityState,
+    required this.dao,
+    super.key,
+  });
+
   final C? entity;
   final String entityName;
   final Dao<C> dao;
-
   final Widget editor;
   final NestedEntityState<C> entityState;
   final Future<void> Function(C? entity) onInsert;
@@ -38,54 +39,55 @@ class NestedEntityEditScreenState<C extends Entity<C>, P extends Entity<P>>
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
-        title: Text(widget.entity != null
-            ? 'Edit ${widget.entityName}'
-            : 'Add ${widget.entityName}'),
-      ),
-      body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
+        appBar: AppBar(
+          title: Text(widget.entity != null
+              ? 'Edit ${widget.entityName}'
+              : 'Add ${widget.entityName}'),
+        ),
+        body: Column(
+          children: [
+            _commandButtons(context),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(16),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      HMBButton(
-                          label: 'Save & Close',
-                          onPressed: () async => _save(close: true)),
-                      const SizedBox(width: 5),
-                      HMBButton(
-                        onPressed: _save,
-                        label: 'Save',
-                      ),
-                      const SizedBox(width: 5),
-                      HMBButton(
-                        onPressed: () => Navigator.of(context).pop(),
-                        label: 'Cancel',
-                      ),
+                      /// Inject the entity specific editor.
+                      widget.editor,
                     ],
                   ),
-
-                  /// Inject the entity specific editor.
-                  widget.editor,
-
-                  /// Save /Cancel Buttons
-                  const SizedBox(height: 16),
-                ],
+                ),
               ),
             ),
-          )));
+          ],
+        ),
+      );
+
+  Padding _commandButtons(BuildContext context) => Padding(
+      padding: const EdgeInsets.all(16),
+      child: Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
+        HMBButton(
+            label: 'Save & Close', onPressed: () async => _save(close: true)),
+        const SizedBox(width: 5),
+        HMBButton(
+          onPressed: _save,
+          label: 'Save',
+        ),
+        const SizedBox(width: 5),
+        HMBButton(
+          onPressed: () => Navigator.of(context).pop(),
+          label: 'Cancel',
+        ),
+      ]));
 
   Future<void> _save({bool close = false}) async {
     if (_formKey.currentState!.validate()) {
       final C entity;
       if (widget.entity != null) {
         entity = await widget.entityState.forUpdate(widget.entity!);
-
         await widget.dao.update(entity);
       } else {
         entity = await widget.entityState.forInsert();
