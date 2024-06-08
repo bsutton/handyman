a32c9339a9492df407697b3844a5f1ae923f92bb
Brett Sutton <bsutton@noojee.com.au>
2024-06-08 23:34:05 +1000

    additional dao methods for filter screens.

--- handyman/lib/dao/dao_task_status.dart
+++ handyman/lib/dao/dao_task_status.dart
@@ -1,9 +1,30 @@
+import 'package:strings/strings.dart';
+
 import '../entity/task_status.dart';
 import 'dao.dart';
 
 class DaoTaskStatus extends Dao<TaskStatus> {
   @override
   String get tableName => 'task_status';
+
+  /// search for jobs given a user supplied filter string.
+  Future<List<TaskStatus>> getByFilter(String? filter) async {
+    final db = getDb();
+
+    if (Strings.isBlank(filter)) {
+      return getAll();
+    }
+
+    final likeArg = '''%$filter%''';
+    final data = await db.rawQuery('''
+select ts.*
+from task_status ts 
+where ts.name like ?
+or ts.description like ?
+''', [likeArg, likeArg]);
+
+    return toList(data);
+  }
 
   @override
   TaskStatus fromMap(Map<String, dynamic> map) => TaskStatus.fromMap(map);
