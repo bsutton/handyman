3d644401e49e5db428e276573da1ec53b81c9514
Brett Sutton <bsutton@noojee.com.au>
2024-06-09 11:22:14 +1000

    changed droplist to a formfield

--- handyman/lib/widgets/hmb_droplist.dart
+++ handyman/lib/widgets/hmb_droplist.dart
@@ -3,28 +3,49 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 import 'hmb_droplist_dialog.dart';
+import 'labeled_container.dart';
 
-class HMBDroplist<T> extends StatefulWidget {
-  const HMBDroplist({
+class HMBDroplist<T> extends FormField<T> {
+  HMBDroplist({
     required this.initialItem,
     required this.items,
     required this.format,
     required this.onChanged,
     required this.title,
+    this.required = true,
     super.key,
-  });
+  }) : super(
+          builder: (state) => _HMBDroplistContent<T>(
+            state: state,
+            initialItem: initialItem,
+            items: items,
+            format: format,
+            onChanged: onChanged,
+            title: title,
+          ),
+          validator: (value) {
+            if (required && value == null) {
+              return 'Required';
+            }
+            return null;
+          },
+        );
+
   final Future<T?> Function() initialItem;
   final Future<List<T>> Function(String? filter) items;
   final String Function(T) format;
   final void Function(T) onChanged;
   final String title;
+  final bool required;
 
   @override
-  // ignore: library_private_types_in_public_api
-  _HMBDroplistState<T> createState() => _HMBDroplistState<T>();
+  FormFieldState<T> createState() => _HMBDroplistState<T>();
 }
 
-class _HMBDroplistState<T> extends State<HMBDroplist<T>> {
+class _HMBDroplistState<T> extends FormFieldState<T> {
+  @override
+  HMBDroplist<T> get widget => super.widget as HMBDroplist<T>;
+
   T? _selectedItem;
   bool _loading = true;
 
@@ -42,52 +63,93 @@ class _HMBDroplistState<T> extends State<HMBDroplist<T>> {
   }
 
   @override
-  Widget build(BuildContext context) => GestureDetector(
-        onTap: () async {
-          final selectedItem = await showDialog<T>(
-            context: context,
-            builder: (context) => HMBDroplistDialog<T>(
-              getItems: widget.items,
-              formatItem: widget.format,
-              title: widget.title,
-              selectedItem: _selectedItem,
-            ),
-          );
+  void didChange(T? value) {
+    super.didChange(value);
+    _selectedItem = value;
+  }
 
-          if (selectedItem != null) {
-            if (mounted) {
-              setState(() {
-                _selectedItem = selectedItem;
-              });
-            }
-            widget.onChanged(selectedItem);
-          }
-        },
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 16,
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(4),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  if (_loading)
-                    const CircularProgressIndicator()
-                  else
-                    Text(_selectedItem != null
-                        ? widget.format(_selectedItem as T)
-                        : widget.title),
-                  const Icon(Icons.arrow_drop_down),
-                ],
-              ),
-            ),
-          ],
-        ),
+  @override
+  Widget build(BuildContext context) => _HMBDroplistContent<T>(
+        state: this,
+        initialItem: widget.initialItem,
+        items: widget.items,
+        format: widget.format,
+        onChanged: widget.onChanged,
+        title: widget.title,
       );
+}
+
+class _HMBDroplistContent<T> extends StatelessWidget {
+  const _HMBDroplistContent({
+    required this.state,
+    required this.initialItem,
+    required this.items,
+    required this.format,
+    required this.onChanged,
+    required this.title,
+  });
+
+  final FormFieldState<T> state;
+  final Future<T?> Function() initialItem;
+  final Future<List<T>> Function(String? filter) items;
+  final String Function(T) format;
+  final void Function(T) onChanged;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final state = this.state as _HMBDroplistState<T>;
+    return GestureDetector(
+      onTap: () async {
+        final selectedItem = await showDialog<T>(
+          context: context,
+          builder: (context) => HMBDroplistDialog<T>(
+            getItems: items,
+            formatItem: format,
+            title: title,
+            selectedItem: state._selectedItem,
+          ),
+        );
+
+        if (selectedItem != null) {
+          state.didChange(selectedItem);
+          onChanged(selectedItem);
+        }
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 16,
+          ),
+          LabeledContainer(
+            labelText: title,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                if (state._loading)
+                  const CircularProgressIndicator()
+                else
+                  Text(state._selectedItem != null
+                      ? format(state._selectedItem as T)
+                      : 'Select a $title'),
+                const Icon(Icons.arrow_drop_down),
+              ],
+            ),
+          ),
+          if (state.hasError)
+            Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: Text(
+                state.errorText ?? '',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.error,
+                  fontSize: 12,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
 }
