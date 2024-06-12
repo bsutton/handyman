65c26d98bd3ebdb5e1820673db14dd2d2076a886
Brett Sutton <bsutton@noojee.com.au>
2024-06-13 07:20:19 +1000

    added lastWhereOrNull

--- handyman/lib/util/list_ex.dart
+++ handyman/lib/util/list_ex.dart
@@ -7,4 +7,24 @@ extension ListEx<T> on List<T> {
     }
     return null;
   }
+
+  T? lastWhereOrNull(bool Function(T element) test) {
+    final iterator = this.iterator;
+    // Potential result during first loop.
+    T result;
+    do {
+      if (!iterator.moveNext()) {
+        return null;
+      }
+      result = iterator.current;
+    } while (!test(result));
+    // Now `result` is actual result, unless a later one is found.
+    while (iterator.moveNext()) {
+      final current = iterator.current;
+      if (test(current)) {
+        result = current;
+      }
+    }
+    return result;
+  }
 }
