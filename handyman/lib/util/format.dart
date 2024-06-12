d689442363b41fa13f1eceba50ca6ed2c3785b75
Brett Sutton <bsutton@noojee.com.au>
2024-06-13 07:21:24 +1000

    added formatters for duration and datetime

--- handyman/lib/util/format.dart
+++ handyman/lib/util/format.dart
@@ -1,4 +1,14 @@
 import 'package:date_time_format/date_time_format.dart';
 
 String formatDate(DateTime dateTime) =>
+    DateTimeFormat.format(dateTime, format: 'D, M j');
+
+String formatDateTime(DateTime dateTime) =>
     DateTimeFormat.format(dateTime, format: 'D, M j, H:i');
+
+String formatDuration(Duration duration) {
+  final hours = duration.inHours;
+  final minutes = duration.inMinutes.remainder(60);
+
+  return '${hours}h ${minutes}m';
+}
