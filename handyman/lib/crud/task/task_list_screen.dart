38df7025ebc8ee291b454bf253c382697ab527a3
Brett Sutton <bsutton@noojee.com.au>
2024-06-13 07:57:31 +1000

    experiements with time entry.

--- handyman/lib/crud/task/task_list_screen.dart
+++ handyman/lib/crud/task/task_list_screen.dart
@@ -3,35 +3,46 @@ import 'package:future_builder_ex/future_builder_ex.dart';
 
 import '../../dao/dao_task.dart';
 import '../../dao/dao_task_status.dart';
+import '../../dao/dao_time_entry.dart';
 import '../../entity/job.dart';
 import '../../entity/task.dart';
+import '../../entity/time_entry.dart';
 import '../../widgets/hmb_text.dart';
 import '../base_nested/nested_list_screen.dart';
-import 'task_edit_screen.dart';
+import '../task/task_edit_screen.dart';
 
-class TaskListScreen extends StatelessWidget {
-  const TaskListScreen({required this.parent, super.key});
-
+class TaskListScreen extends StatefulWidget {
+  const TaskListScreen(
+      {required this.parent, required this.extended, super.key});
   final Parent<Job> parent;
+  final bool extended;
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _TaskListScreenState createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  final Map<int, bool> _taskTimers = {};
 
   @override
   Widget build(BuildContext context) => NestedEntityListScreen<Task, Job>(
-      parent: parent,
-      pageTitle: 'Tasks',
-      parentTitle: 'Job',
-      dao: DaoTask(),
-      // ignore: discarded_futures
-      fetchList: () => DaoTask().getTasksByJob(parent.parent!),
-      title: (entity) => Text(entity.name),
-      onEdit: (task) => TaskEditScreen(job: parent.parent!, task: task),
-      onDelete: (task) async => DaoTask().delete(task!.id),
-      onInsert: (task) async => DaoTask().insert(task!),
-      details: (task) =>
-          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        parent: widget.parent,
+        pageTitle: 'Tasks',
+        parentTitle: 'Job',
+        dao: DaoTask(),
+        // ignore: discarded_futures
+        fetchList: () => DaoTask().getTasksByJob(widget.parent.parent!),
+        title: (entity) => Text(entity.name),
+        onEdit: (task) => TaskEditScreen(job: widget.parent.parent!, task: task),
+        onDelete: (task) async => DaoTask().delete(task!.id),
+        onInsert: (task) async => DaoTask().insert(task!),
+        details: (task) => 
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             FutureBuilderEx(
-                // ignore: discarded_futures
-                future: DaoTaskStatus().getById(task.taskStatusId),
-                builder: (context, status) => Text(status?.name ?? 'Not Set')),
+              // ignore: discarded_futures
+              future: DaoTaskStatus().getById(task.taskStatusId),
+              builder: (context, status) => Text(status?.name ?? 'Not Set')),
             FutureBuilderEx(
               // ignore: discarded_futures
               future: DaoTask().getTaskStatistics(task),
@@ -45,5 +56,41 @@ class TaskListScreen extends StatelessWidget {
               ),
             ),
             Text('Completed: ${task.completed}'),
-          ]));
+            IconButton(
+              icon: Icon(_taskTimers[task.id] ?? false
+                  ? Icons.stop
+                  : Icons.play_arrow),
+              onPressed: () async => _toggleTimer(task),
+            ),
+          ],
+        ),
+      );
+
+  /// Start/Stop the Time entry timer.
+  Future<void> _toggleTimer(Task task) async {
+    if (_taskTimers[task.id] ?? false) {
+      // Stop timer
+      final daoTimeEntry = DaoTimeEntry();
+      final timeEntries = await daoTimeEntry.getByTask(task);
+      final ongoingEntry =
+          timeEntries.firstWhere((entry) => entry.endTime == null);
+      final updatedEntry = TimeEntry.forUpdate(
+          entity: ongoingEntry,
+          taskId: task.id,
+          startTime: ongoingEntry.startTime,
+          endTime: DateTime.now());
+      await daoTimeEntry.update(updatedEntry);
+      setState(() {
+        _taskTimers[task.id] = false;
+      });
+    } else {
+      // Start timer
+      final newEntry =
+          TimeEntry.forInsert(taskId: task.id, startTime: DateTime.now());
+      await DaoTimeEntry().insert(newEntry);
+      setState(() {
+        _taskTimers[task.id] = true;
+      });
+    }
+  }
 }
