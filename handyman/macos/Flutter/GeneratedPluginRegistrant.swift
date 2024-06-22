824da479e97e698ddc3df332b0564ea6652025f7
Brett Sutton <bsutton@onepub.dev>
2024-06-22 15:19:28 +1000

    wip: xero integration

--- handyman/macos/Flutter/GeneratedPluginRegistrant.swift
+++ handyman/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import flutter_appauth
+import flutter_secure_storage_macos
 import google_sign_in_ios
 import path_provider_foundation
 import sqflite
@@ -13,6 +15,8 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterAppauthPlugin.register(with: registry.registrar(forPlugin: "FlutterAppauthPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
