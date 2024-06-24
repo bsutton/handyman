f099dcfdc9810093a1e2f93e0f055c8ae5b68658
Brett Sutton <bsutton@noojee.com.au>
2024-06-25 06:59:03 +1000

    added missing sim_card_no to system table.

--- handyman/assets/sql/upgrade_scripts/v8.sql
+++ handyman/assets/sql/upgrade_scripts/v8.sql
@@ -1,5 +1,5 @@
 alter table time_entry add column notes TEXT;
 
-ALTER TABLE system
-ADD COLUMN xero_client_id VARCHAR(255),
-ADD COLUMN xero_client_secret VARCHAR(255);
+ALTER TABLE system ADD COLUMN xero_client_id VARCHAR(255);
+ALTER TABLE system ADD COLUMN xero_client_secret VARCHAR(255);
+ALTER TABLE system ADD COLUMN sim_card_no int;
