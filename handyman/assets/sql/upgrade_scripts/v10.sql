a0bd2271bcbd0d6a4517db22b647dd82f82a51e8
Brett Sutton <bsutton@noojee.com.au>
2024-06-27 12:42:17 +1000

    had a problem with production db not matching schema - now fixed.

--- handyman/assets/sql/upgrade_scripts/v10.sql
+++ handyman/assets/sql/upgrade_scripts/v10.sql
@@ -1,5 +1,4 @@
-ALTER TABLE Supplier
-ADD COLUMN businessNumber TEXT,
-ADD COLUMN description TEXT,
-ADD COLUMN bsb TEXT ,
-ADD COLUMN accountNumber TEXT ;
+
+ALTER TABLE Supplier ADD COLUMN businessNumber TEXT;
+ALTER TABLE Supplier ADD COLUMN bsb TEXT ;
+ALTER TABLE Supplier ADD COLUMN accountNumber TEXT ;
--- handyman/assets/sql/upgrade_scripts/v8.sql
+++ handyman/assets/sql/upgrade_scripts/v8.sql
@@ -1,6 +1,2 @@
 alter table time_entry add column notes TEXT;
 
-ALTER TABLE system ADD COLUMN xero_client_id VARCHAR(255);
-ALTER TABLE system ADD COLUMN xero_client_secret VARCHAR(255);
-ALTER TABLE system ADD COLUMN sim_card_no int;
-
